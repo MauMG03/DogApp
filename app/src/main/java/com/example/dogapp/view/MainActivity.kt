1b572afe63c2df4fef16e5390e3fffe3a4d54ea8
MauMG03 <mauricio.munoz.gutierrez@correounivalle.edu.co>
2024-05-06 15:41:42 -0500

    Creacion de estructura de archivos de kotlin

--- app/src/main/java/com/example/dogapp/view/MainActivity.kt
+++ app/src/main/java/com/example/dogapp/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.dogapp
+package com.example.dogapp.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.dogapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
