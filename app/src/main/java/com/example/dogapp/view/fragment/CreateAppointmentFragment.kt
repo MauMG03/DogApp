98246a09d1c2ab76b972a1bc4e890578b076f5de
TheCryss <38475424+TheCryss@users.noreply.github.com>
2024-05-08 00:04:12 -0500

    intial styles and toolbar

---
+++ app/src/main/java/com/example/dogapp/view/fragment/CreateAppointmentFragment.kt
@@ -0,0 +1,32 @@
+package com.example.dogapp.view.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.dogapp.R
+import com.example.dogapp.databinding.FragmentCreateAppointmentBinding
+
+
+class CreateAppointmentFragment : Fragment() {
+    private lateinit var binding: FragmentCreateAppointmentBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+//        return inflater.inflate(R.layout.fragment_create_appointment, container, false)
+        binding = FragmentCreateAppointmentBinding.inflate(inflater)
+        binding.lifecycleOwner = this
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    private fun controladores(){
+
+    }
+}
